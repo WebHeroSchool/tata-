700431e08e3ae5463ca26eb2f179f7d0b343e630
Татьяна Соснина <tatyana-sosnina@mail.ru>
2021-01-13 17:00:50 +0700

    style

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ItemList from '../ItemList/ItemList';
 import InputItem from '../InputItem/InputItem';
 import Footer from '../Footer/Footer';
+import './App.css';
 
 const todoItem = 'Написать новое приложение';
 
@@ -19,8 +20,8 @@ const App = () => {
     ];
 
   return (
-  <div>
-      <h1>Важные дела!</h1>
+  <div className="wrap">
+      <h1 className="wrap__title">Важные дела!</h1>
       <InputItem />
       <ItemList items={items} />
       <Footer count={3} />
